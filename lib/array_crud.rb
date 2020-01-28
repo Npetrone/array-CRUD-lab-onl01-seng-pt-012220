681691f8c7000e3717e808010bb841f389d07b4e
Nicholas Petrone <nickpetrone92@gmail.com>
2020-01-28 03:46:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,13 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  cats = ["grumpy cat", "Simba", "Nala"]
-  cats << "Scar"
+  
 end
 
 def add_element_to_start_of_array(array, element)
-  cats = ["grumpy cat", "Simba", "Nala"]
-  cats.unshift("Scar")
+  
 end
 
 def remove_element_from_end_of_array(array)
