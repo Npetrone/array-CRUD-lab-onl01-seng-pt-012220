556837687202e3557ff5ffb388bb0d8f463b2281
Nicholas Petrone <nickpetrone92@gmail.com>
2020-01-28 03:54:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ def remove_element_from_end_of_array(array)
 def remove_element_from_start_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
   array.shift
-  end
+end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
